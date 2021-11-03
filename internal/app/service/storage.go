45674266b3bc2bf4fa2496a1693cce32e3751ec2
Serg_Kotovsky <rookulab@mail.ru>
2021-11-03 23:52:56 +0300

    change app structure

--- internal/app/service/storage.go
+++ internal/app/service/storage.go
@@ -1,13 +1,16 @@
 package service
 
-import "github.com/gofrs/uuid"
+import (
+	"github.com/google/uuid"
+	"github.com/seggga/observability/internal/pkg/model"
+)
 
 type Storage interface {
 	Init()
 	CreateUser(*model.User) (*uuid.UUID, error) // creates a new user
 	DeleteUser(*uuid.UUID) error                // deletes a new user
 	CreateLink(*model.Link) error               // creates a new redirect link
-	GetLink(string) (*model.LinkData, error)    // retrieves all data that corresponds to the short link
+	GetLink(string) (*model.Link, error)        // retrieves all data that corresponds to the short link
 	Redirect(string) (string, error)            // retrieves long URL from database to produce redirect
 	DeleteLink(string, *uuid.UUID) error        // deletes the link specified
 	Close()                                     // close connection to the storage (database / file / ....)
