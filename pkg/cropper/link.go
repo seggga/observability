02e5bce3a8e1d213f8a3c0f4bc294224978372f8
Serg_Kotovsky <rookulab@mail.ru>
2021-11-06 22:00:14 +0300

    decapitalize json tags

--- pkg/cropper/link.go
+++ pkg/cropper/link.go
@@ -3,9 +3,9 @@ package cropper
 import "github.com/gofrs/uuid"
 
 type Link struct {
-	Short       string    `json:"Short"`
-	Long        string    `json:"Long"`
-	Owner       uuid.UUID `json:"Owner"`
-	Count       int64     `json:"Count"`
-	Description string    `json:"Description"`
+	Short       string    `json:"short"`
+	Long        string    `json:"long"`
+	Owner       uuid.UUID `json:"owner"`
+	Count       int64     `json:"count"`
+	Description string    `json:"description"`
 }
