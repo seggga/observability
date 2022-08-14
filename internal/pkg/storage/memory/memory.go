67a1188563c232adc77be496fe0bef9fd914437a
Serg Kotovsky <rokulab@mail.ru>
2022-08-15 00:20:10 +0300

    add user storage

--- internal/pkg/storage/memory/memory.go
+++ internal/pkg/storage/memory/memory.go
@@ -2,53 +2,85 @@ package memory
 
 import (
 	"fmt"
+	"log"
 	"sync"
 
+	"github.com/google/uuid"
 	"github.com/seggga/observability/internal/pkg/model"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type mem struct {
-	storage map[string]model.Link
-	mutex   sync.RWMutex
+	linkStor  map[string]model.Link
+	linkMutex sync.RWMutex
+
+	userStore map[string]model.User
+	userMutex sync.RWMutex
 }
 
 // New creates a new in-memory repository (a map with predefined data)
 func New() *mem {
-	mapStorage := make(map[string]model.Link)
-	// predefine data 1
-	mapStorage["asdf"] = model.Link{
+	// users
+	userStore := make(map[string]model.User, 2)
+	pass1, err := bcrypt.GenerateFromPassword([]byte("pass1"), 0)
+	if err != nil {
+		log.Fatal("error creating initial users")
+	}
+	userStore["user1"] = model.User{
+		ID:       uuid.New(),
+		Name:     "user1",
+		PassHash: string(pass1),
+	}
+
+	pass2, err := bcrypt.GenerateFromPassword([]byte("pass2"), 0)
+	if err != nil {
+		log.Fatal("error creating initial users")
+	}
+	userStore["user2"] = model.User{
+		ID:       uuid.New(),
+		Name:     "user2",
+		PassHash: string(pass2),
+	}
+
+	// links
+	linkStor := make(map[string]model.Link, 2)
+	// predefine link #1
+	linkStor["asdf"] = model.Link{
 		Short:       "asdf",
 		Long:        "https://google.com",
 		Count:       0,
 		Description: "a very useful link",
+		Owner:       userStore["user1"].ID,
 	}
-	// predefine data 2
-	mapStorage["qwerty"] = model.Link{
+	// predefine link #2
+	linkStor["qwerty"] = model.Link{
 		Short:       "qwerty",
 		Long:        "https://yandex.ru",
 		Count:       0,
 		Description: "one more useful link",
+		Owner:       userStore["user2"].ID,
 	}
 
 	return &mem{
-		storage: mapStorage,
+		linkStor:  linkStor,
+		userStore: userStore,
 	}
 }
 
 // CreateLink adds new link data to the map
 func (m *mem) CreateLink(link *model.Link) error {
-	m.mutex.Lock()
-	m.storage[link.Short] = *link
-	m.mutex.Unlock()
+	m.linkMutex.Lock()
+	m.linkStor[link.Short] = *link
+	m.linkMutex.Unlock()
 
 	return nil
 }
 
 // GetLink retrieves specified link from the storage
 func (m *mem) GetLink(short string) (*model.Link, error) {
-	m.mutex.RLock()
-	link := m.storage[short]
-	m.mutex.RUnlock()
+	m.linkMutex.RLock()
+	link := m.linkStor[short]
+	m.linkMutex.RUnlock()
 
 	// check for valid data
 	if link.Long == "" {
@@ -64,18 +96,18 @@ func (m *mem) Close() {
 
 // IsSet checks if an element exist in the storage
 func (m *mem) IsSet(short string) bool {
-	m.mutex.RLock()
-	_, ok := m.storage[short]
-	m.mutex.RUnlock()
+	m.linkMutex.RLock()
+	_, ok := m.linkStor[short]
+	m.linkMutex.RUnlock()
 
 	return ok
 }
 
 // DeleteLink deletes link data from the map
 func (m *mem) DeleteLink(short string) error {
-	m.mutex.Lock()
-	delete(m.storage, short)
-	m.mutex.Unlock()
+	m.linkMutex.Lock()
+	delete(m.linkStor, short)
+	m.linkMutex.Unlock()
 
 	return nil
 }
