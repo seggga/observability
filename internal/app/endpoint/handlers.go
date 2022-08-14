d48eff91bef0e970a41451c3a6dd5130a35a0b84
Serg Kotovsky <rokulab@mail.ru>
2022-08-15 00:19:06 +0300

    use logger in http.handlers

--- internal/app/endpoint/handlers.go
+++ internal/app/endpoint/handlers.go
@@ -11,140 +11,158 @@ import (
 	"github.com/seggga/observability/pkg/cropper"
 )
 
-func home() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "welcome to cropper!")
-	}
-}
-func redirect(s service) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// define shortID from users query
-		params := mux.Vars(r)
-		short := params["short"]
-		// defint corresponding long URL from database
-		long, err := s.Resolve(short)
-		if err != nil {
-			err = fmt.Errorf("cannot obtain ling URI: %w", err)
-			http.Error(w, "cannot obtain ling URI", http.StatusInsufficientStorage)
-			// slogger.Debugf("resolving error %w", err)
-			// JSONError(rw, err, http.StatusBadRequest)
-			return
-		}
-		// slogger.Debugf("successful redirect %s -> %s", shortID, longURL)
+func (rs *RESTService) home(w http.ResponseWriter, r *http.Request) {
+	rs.l.Debug("home func called")
 
-		// implement redirect
-		http.Redirect(w, r, long, http.StatusPermanentRedirect)
+	fmt.Fprint(w, "welcome to cropper!")
+}
+
+func (rs *RESTService) redirect(w http.ResponseWriter, r *http.Request) {
+	rs.l.Debug("redirect func called")
+
+	// define shortID from users query
+	params := mux.Vars(r)
+	short := params["short"]
+	// define corresponding long URL from database
+	long, err := rs.s.Resolve(short)
+	if err != nil {
+		err = fmt.Errorf("cannot obtain ling URI: %w", err)
+		rs.l.Error(err.Error())
+
+		http.Error(w, "cannot obtain ling URI", http.StatusInsufficientStorage)
+		// JSONError(rw, err, http.StatusBadRequest)
+		return
 	}
+
+	rs.l.Sugar().Debugf("successful redirect %s -> %s", short, long)
+
+	http.Redirect(w, r, long, http.StatusPermanentRedirect)
 }
 
 // authMiddleware checks user's authorization
-func authMiddleware(h http.HandlerFunc) http.HandlerFunc {
+func (rs *RESTService) authMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			// check authorization
-			_, _, ok := r.BasicAuth()
+			user, pass, ok := r.BasicAuth()
 			if !ok {
-				// err = fmt.Errorf("unauthorized access")
+				err := fmt.Errorf("unauthorized access")
+				rs.l.Error(err.Error())
 				http.Error(w, "unauthorized access", http.StatusUnauthorized)
-				// slogger.Debug(err)
 				// JSONError(rw, err, http.StatusBadRequest)
 				return
 			}
-			h.ServeHTTP(w, r)
+
+			next.ServeHTTP(w, r)
 		},
 	)
 }
 
-func newLink(s service) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func (rs *RESTService) newLink(w http.ResponseWriter, r *http.Request) {
+	rs.l.Debug("newLink func called")
 
-		// obtain request body
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			err = fmt.Errorf("unable to parse request body %w", err)
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			// slogger.Debug(err)
-			// JSONError(rw, err, http.StatusBadRequest)
-			return
-		}
-		defer r.Body.Close()
+	// obtain request body
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		err = fmt.Errorf("unable to parse request body %w", err)
 
-		// compose dataset
-		link := new(cropper.Link)
-		err = json.Unmarshal(body, link)
-		if err != nil {
-			err = fmt.Errorf("unable to unmarshal JSON %w", err)
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			// slogger.Debug(err)
-			// JSONError(rw, err, http.StatusBadRequest)
-			return
-		}
+		rs.l.Error(err.Error())
 
-		// call service level
-		err = s.NewLink(link)
-		if err != nil {
-			err = fmt.Errorf("unable to create link pair: %w", err)
-			http.Error(w, "unable to create link pair", http.StatusInsufficientStorage)
-			// slogger.Debug(err)
-			// JSONError(rw, err, http.StatusBadRequest)
-			return
-		}
-
-		// slogger.Infof("a new short ID added to database %+v", link)
-		// output data
-		w.Header().Set("Application", "Cropper")
-		w.WriteHeader(http.StatusOK)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		// JSONError(rw, err, http.StatusBadRequest)
+		return
 	}
+	defer r.Body.Close()
+
+	// compose dataset
+	link := new(cropper.Link)
+	err = json.Unmarshal(body, link)
+	if err != nil {
+		err = fmt.Errorf("unable to unmarshal JSON %w", err)
+
+		rs.l.Error(err.Error())
+
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		// JSONError(rw, err, http.StatusBadRequest)
+		return
+	}
+
+	// call service level
+	err = rs.s.NewLink(link)
+	if err != nil {
+		err = fmt.Errorf("unable to create link pair: %w", err)
+
+		rs.l.Error(err.Error())
+
+		http.Error(w, "unable to create link pair", http.StatusInsufficientStorage)
+		// JSONError(rw, err, http.StatusBadRequest)
+		return
+	}
+
+	rs.l.Sugar().Infof("a new short ID added to database %+v", link)
+
+	// output data
+	w.Header().Set("Application", "Cropper")
+	w.WriteHeader(http.StatusOK)
 }
 
-func deleteLink(s service) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// obtain request body
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			err = fmt.Errorf("unable to parse request body %w", err)
-			http.Error(w, "unable to parse request body", http.StatusBadRequest)
-			// slogger.Debug(err)
-			// JSONError(rw, err, http.StatusBadRequest)
-			return
-		}
-		defer r.Body.Close()
+func (rs *RESTService) deleteLink(w http.ResponseWriter, r *http.Request) {
+	rs.l.Debug("deleteLink func called")
 
-		// compose dataset
-		link := new(cropper.Link)
-		err = json.Unmarshal(body, link)
-		if err != nil {
-			err = fmt.Errorf("unable to unmarshal JSON %w", err)
-			http.Error(w, "unable to unmarshal JSON", http.StatusBadRequest)
-			// slogger.Debug(err)
-			// JSONError(rw, err, http.StatusBadRequest)
-			return
-		}
+	// obtain request body
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		err = fmt.Errorf("unable to parse request body %w", err)
 
-		if link.Short == "" {
-			err = fmt.Errorf("the short link is empty, nothing to delete")
-			http.Error(w, "the short link is empty, nothing to delete", http.StatusBadRequest)
-			// slogger.Debug(err)
-			// JSONError(rw, err, http.StatusBadRequest)
-			return
-		}
+		rs.l.Error(err.Error())
 
-		// TODO - execute same check for the user (mean check authorized user deletes his own link)
-
-		// call service level
-		userID := uuid.New()
-		err = s.DeleteLink(link.Short, &userID)
-		if err != nil {
-			err = fmt.Errorf("unable to create link pair: %w", err)
-			http.Error(w, "unable to create link pair", http.StatusInsufficientStorage)
-			// slogger.Debug(err)
-			// JSONError(rw, err, http.StatusBadRequest)
-			return
-		}
-
-		// slogger.Infof("a new short ID added to database %+v", link)
-		// output data
-		w.Header().Set("Application", "Cropper")
-		w.WriteHeader(http.StatusOK)
+		http.Error(w, "unable to parse request body", http.StatusBadRequest)
+		// JSONError(rw, err, http.StatusBadRequest)
+		return
 	}
+	defer r.Body.Close()
+
+	// compose dataset
+	link := new(cropper.Link)
+	err = json.Unmarshal(body, link)
+	if err != nil {
+		err = fmt.Errorf("unable to unmarshal JSON %w", err)
+
+		rs.l.Error(err.Error())
+
+		http.Error(w, "unable to unmarshal JSON", http.StatusBadRequest)
+		// JSONError(rw, err, http.StatusBadRequest)
+		return
+	}
+
+	if link.Short == "" {
+		err = fmt.Errorf("the short link is empty, nothing to delete")
+
+		rs.l.Error(err.Error())
+
+		http.Error(w, "the short link is empty, nothing to delete", http.StatusBadRequest)
+		// JSONError(rw, err, http.StatusBadRequest)
+		return
+	}
+
+	// TODO - execute same check for the user (mean check authorized user deletes his own link)
+
+	// call service level
+	userID := uuid.New()
+	err = rs.s.DeleteLink(link.Short, &userID)
+	if err != nil {
+		err = fmt.Errorf("unable to create link pair: %w", err)
+
+		rs.l.Error(err.Error())
+
+		http.Error(w, "unable to create link pair", http.StatusInsufficientStorage)
+		// JSONError(rw, err, http.StatusBadRequest)
+		return
+	}
+
+	rs.l.Sugar().Infof("a new short ID added to database %+v", link)
+
+	// output data
+	w.Header().Set("Application", "Cropper")
+	w.WriteHeader(http.StatusOK)
 }
